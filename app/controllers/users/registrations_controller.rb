ca2a781b135eca3beaf142764b8e2186287a3b6b
nmasalkov <nikitamasalkov.tang@gmail.com>
2024-07-12 18:38:35 +0200

    limit daily users

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,14 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
+  before_action :check_if_too_many_registrations
 
   protected
+
+  def check_if_too_many_registrations
+    lately_created_users = User.where(created_at: Date.yesterday..).count
+    raise 'too many users!' if lately_created_users > 5
+  end
 
   def configure_sign_up_params
     devise_parameter_sanitizer.permit(:sign_up, keys: [:login, :password, :password_confirmation])
